089a951900bd16fd0dcb326a2eea6755f4245c20
Patrick Fraley <patrick@fraley.de>
2026-01-14 17:23:57 +0100

    Add VSCode

--- build_files/build.sh
+++ build_files/build.sh
@@ -17,6 +17,14 @@ dnf5 install -y libreoffice libreoffice-langpack-de
 dnf5 install -y distrobox
 dnf5 install -y dotnet-sdk-10.0
 
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
+echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
+
+dnf5 check-update
+dnf5 install -y code
+
+rm /etc/yum.repos.d/vscode.repo
+
 # Use a COPR Example:
 #
 #dnf5 -y copr enable che/nerd-fonts
