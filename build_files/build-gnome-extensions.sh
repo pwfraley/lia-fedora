7a6f00ee244b02969550a77080d5f49efb740cfe
Patrick Fraley <patrick@fraley.de>
2026-01-18 11:35:50 +0100

    fix installation for extension

--- build_files/build-gnome-extensions.sh
+++ build_files/build-gnome-extensions.sh
@@ -18,7 +18,7 @@ git clone https://github.com/stuarthayhurst/alphabetical-grid-extension.git ./Al
 cd AlphabeticalAppGrid@stuarthayhurst
 make build
 unzip build/AlphabeticalAppGrid@stuarthayhurst.shell-extension.zip /usr/share/gnome-shell/extensions/
-glib-compile-schemas /usr/share/gnome-shell/AlphabeticalAppGrid@stuarthayhurst/shemas/
+glib-compile-schemas /usr/share/gnome-shell/extensions/AlphabeticalAppGrid@stuarthayhurst/shemas/
 cd ..
 rm -rf AlphabeticalAppGrid@stuarthayhurst
 
