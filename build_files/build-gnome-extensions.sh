162b3eaf949fc32e14a4bb888d176e5eafaa7fb0
Patrick Fraley <patrick@fraley.de>
2026-01-18 09:39:05 +0100

    Fix stuff

---
+++ build_files/build-gnome-extensions.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/bash
+
+set -eoux pipefail
+
+echo "::group:: ===$(basename "$0")==="
+
+echo ":: Installing Fedora Extensions"
+dnf5 install -y gnome-shell-extension-pop-shell gnome-shell-extension-user-theme
+
+echo ":: Building Extensions"
+# Install tooling
+dnf5 -y install glib2-devel meson sassc cmake dbus-devel
+
+# Build Extensions
+
+# Alphabetical App Grid
+make -C /usr/share/gnome-shell/extensions/alphabetical-grid-extension
+unzip -o /usr/share/gnome-shell/extensions/alphabetical-grid-extension/build/AlphabeticalAppGrid@stuarthayhurst.shell-extension.zip -d /usr/share/gnome-shell/extensions/AlphabeticalAppGrid@stuarthayhurst
+glib-compile-schemas --strict /usr/share/gnome-shell/extensions/AlphabeticalAppGrid@stuarthayhurst/schemas
+rm -rf /usr/share/gnome-shell/extensions/AlphabeticalAppGrid@stuarthayhurst/build
+
+
+# Blur My Shell
+make -C /usr/share/gnome-shell/extensions/blur-my-shell@aunetx
+unzip -o /usr/share/gnome-shell/extensions/blur-my-shell@aunetx/build/blur-my-shell@aunetx.shell-extension.zip -d /usr/share/gnome-shell/extensions/blur-my-shell@aunetx
+glib-compile-schemas --strict /usr/share/gnome-shell/extensions/blur-my-shell@aunetx/schemas
+rm -rf /usr/share/gnome-shell/extensions/blur-my-shell@aunetx/build
+
+# Dash to Dock
+make -C /usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com
+glib-compile-schemas --strict /usr/share/gnome-shell/extensions/dash-to-dock@micxgx.gmail.com/schemas
+
+# GSConnect (commented out until G49 support)
+# meson setup --prefix=/usr /usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io /usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/_build
+# meson install -C /usr/share/gnome-shell/extensions/gsconnect@andyholmes.github.io/_build --skip-subprojects
+# GSConnect installs schemas to /usr/share/glib-2.0/schemas and meson compiles them automatically
+
+# Logo Menu
+# xdg-terminal-exec is required for this extension as it opens up terminals using that script
+# install -Dpm0755 -t /usr/bin /usr/share/gnome-shell/extensions/logomenu@aryan_k/distroshelf-helper
+# install -Dpm0755 -t /usr/bin /usr/share/gnome-shell/extensions/logomenu@aryan_k/missioncenter-helper
+# glib-compile-schemas --strict /usr/share/gnome-shell/extensions/logomenu@aryan_k/schemas
+
+# Search Light
+# glib-compile-schemas --strict /usr/share/gnome-shell/extensions/search-light@icedman.github.com/schemas
+
+rm /usr/share/glib-2.0/schemas/gschemas.compiled
+glib-compile-schemas /usr/share/glib-2.0/schemas
+
+# Cleanup
+dnf5 -y remove glib2-devel meson sassc cmake dbus-devel
+rm -rf /usr/share/gnome-shell/extensions/tmp
+
+echo "::endgroup::"
