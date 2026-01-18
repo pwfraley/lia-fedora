b8e387d047bc516e9efafe85f8be2f5899fdf12f
Patrick Fraley <patrick@fraley.de>
2026-01-18 17:03:29 +0100

    noch nen fix

--- build_files/build-gnome-extensions.sh
+++ build_files/build-gnome-extensions.sh
@@ -31,7 +31,7 @@ glib-compile-schemas /usr/share/gnome-shell/extensions/clipboard-indicator@tudmo
 # Color Picker
 git clone https://github.com/tuberry/color-picker.git ./color-picker@tuberry
 cd color-picker@tuberry
-meson setup build -Dtarget=zip && meson -C build
+meson setup build -Dtarget=zip && meson install -C build
 echo $(ls)
 echo $(ls build)
 cd ..
